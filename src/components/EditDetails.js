eccef52cc1490ea87b33f90bac68cae236a40820
knimalaka <kavindanimalaka97@gmail.com>
2021-05-19 15:09:21 +0530

    Basic Edit details page

--- src/components/EditDetails.js
+++ src/components/EditDetails.js
@@ -4,15 +4,10 @@ function EditDetails(name,email,password){
 
 
     return(
-        /*<form onSubmit={submitHandler}>
-            <div className="form-inner">
-            */
+        
 
             <h2>Edit Details Here</h2>
-        /*
-            </div>
-        </form>
-       */
+    
     )
 }
 
