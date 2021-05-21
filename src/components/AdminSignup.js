b0d4ba8d8587b78c8c977d83bf80a3030d0b3748
knimalaka <kavindanimalaka97@gmail.com>
2021-05-21 11:08:13 +0530

    Initializing admin signup page

---
+++ src/components/AdminSignup.js
@@ -0,0 +1,17 @@
+import React, {useState} from "react";
+
+function AdminSignup() {
+     
+    return (
+  
+      <div className="AdminSignup">
+  
+        
+          
+      </div>
+  
+    );
+  }
+  
+  export default AdminSignup;
+  
