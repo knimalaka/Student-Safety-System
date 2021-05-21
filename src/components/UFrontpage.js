146088baf925916b86bd2ca5fd35e9bcefff4bab
knimalaka <kavindanimalaka97@gmail.com>
2021-05-21 14:17:36 +0530

    Error checking of user front page

---
+++ src/components/UFrontpage.js
@@ -0,0 +1,17 @@
+import React, {useState} from "react";
+
+function UserFrontPage() {
+     
+    return (
+  
+      <div className="UserFrontPage">
+  
+        
+          
+      </div>
+  
+    );
+  }
+  
+  export default UserFrontPage;
+  
--- src/components/UserFrontpage.js
+++ src/components/UserFrontpage.js
@@ -1,17 +1,40 @@
-import React, {useState} from "react";
+import React, {useState,Component} from 'react';
+import {useHistory} from "react-router-dom";
+import img1 from '../Images/Main.png';
 
-function UserFrontPage() {
-     
-    return (
-  
-      <div className="UserFrontPage">
-  
+ 
+function UserFrontPage(){
+
+    let history = useHistory();
+
+    return(
         
-          
-      </div>
-  
-    );
-  }
-  
-  export default UserFrontPage;
-  
+        <div className="UserFrontPage">
+
+        <img src={img1} alt=""/>
+
+
+        <div className="Login">
+        <button onClick= {() => { history.push("./LoginForm"); }}>Login</button>
+        </div>
+
+        <div className="Signup">
+        <button onClick= {() => { history.push("./SignupForm"); }}>Signup</button>
+        </div>
+
+        <div className="h2_1">
+        <h2>STUDENT SAFETY</h2>
+        </div>
+
+        <div className="h2_2">
+        <h2>SYSTEM</h2>        
+        </div>
+
+        </div>
+    
+           
+    )
+}
+
+export default UserFrontPage;
+
