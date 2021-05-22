a52841914df388aa1222c6402195f5fa3fbe4930
knimalaka <kavindanimalaka97@gmail.com>
2021-05-22 10:54:41 +0530

    Front page developing

--- src/components/Frontpage.js
+++ src/components/Frontpage.js
@@ -1,18 +1,31 @@
 import React, {useState} from "react";
 import {useHistory} from "react-router-dom";
 
-function FrontPage() {
-     
-    return (
-  
-      <div className="FrontPage">
-  
-        
+function FrontPage() {  
+    
+      let history = useHistory();
+
+      return(
           
-      </div>
+        <div className="FrontPage">  
   
-    );
+          <div className="Parent">
+          <button onClick= {() => { history.push("./UserFrontPage"); }}>Parent</button>
+          </div>
+  
+          <div className="Admin">
+          <button onClick= {() => { history.push("./AdminFrontPage"); }}>Admin</button>
+          </div>
+  
+          <div className="h2_1">
+          <h2>STUDENT SAFETY</h2>
+          </div>
+  
+  
+        </div>
+  
+    )
   }
   
-  export default FrontPage;
+export default FrontPage;
   
