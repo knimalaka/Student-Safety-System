f0a269070fbbd08bf89eb2a767fecd6a444e0e1e
knimalaka <kavindanimalaka97@gmail.com>
2021-05-25 23:19:51 +0530

    Added css to button 1

--- src/components/Frontpage.js
+++ src/components/Frontpage.js
@@ -1,6 +1,8 @@
 import React, {useState} from "react";
 import {useHistory} from "react-router-dom";
 import img2 from '../Images/parent.png';
+import img3 from '../Images/admin2.png';
+
 
 function FrontPage() {  
     
@@ -11,11 +13,11 @@ function FrontPage() {
         <div className="FrontPage">  
   
           <div className="Parent">
-          <button onClick= {() => { history.push("./UserFrontPage"); }}><img src={img2}/></button>
+          <button onClick= {() => { history.push("./UserFrontPage"); }}><img src={img2}/>Parent</button>
           </div>
   
           <div className="Admin">
-          <button onClick= {() => { history.push("./AdminFrontPage"); }}>Admin</button>
+          <button onClick= {() => { history.push("./AdminFrontPage"); }}><img src={img3}/>Admin</button>
           </div>
   
           <div className="h2_1">
