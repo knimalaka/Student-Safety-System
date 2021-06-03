200d292a135bce4785676de6948ed6d5f7fe3245
knimalaka <kavindanimalaka97@gmail.com>
2021-06-03 11:32:19 +0530

    Created Hoeme.css

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import './FrontPage.css';
 import './UserFrontPage.css';
 import './LoginForm.css';
+import './Home.css';
 
 ReactDOM.render(
   <React.StrictMode>
