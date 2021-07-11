889167e4a3f97de4aab18224468d7e38eae6abf3
knimalaka <kavindanimalaka97@gmail.com>
2021-07-11 20:46:52 +0530

    adding styles to signup page

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
+import 'react-phone-number-input/style.css';
+import PhoneInput from 'react-phone-number-input';
+import img4 from '../Images/children4.png';
+
 
 const schema = yup.object().shape({
     username: yup.string().required(),
@@ -14,9 +18,44 @@ const content={
 
     inputs: [
         {
-            label:'Username',
-            name: 'username',
+            label:"Student Name",
+            name: "student Name",
             type: "text",
+        },
+        {
+            label: 'Student ID',
+            name: 'student ID',
+            type: "text",
+        },
+        {
+            label: 'Class (Ex:1A)',
+            name: 'class',
+            type: "text",
+        },
+        {
+            label: 'Date of birth',
+            name: 'date of birth',
+            type: "date",
+        },
+        {
+            label: 'Address',
+            name: 'address',
+            type: "text",
+        },
+        {
+            label: 'Email addresss',
+            name: 'email address',
+            type: "email",
+        },
+        {
+            label: 'Parent Name',
+            name: 'parent Name',
+            type: "text",
+        },
+        {
+            label: 'Phone Number',
+            name: 'phone Number',
+            type: PhoneInput,
         },
         {
             label: 'Password',
@@ -34,8 +73,12 @@ const onSubmit = (data) => console.log(data);
 console.log(errors);
 
     return(
+
         <div className="UserSignupForm">
         <form  onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-inner">
+
+ 
         <h2>Signup</h2>
             {content.inputs.map((input, key) => {
                 return( 
@@ -44,20 +87,29 @@ console.log(errors);
                         <label className="label">{input.label}</label>
                     </p>
                     <p>
+                    <div className="form-group">
+
                         <input 
                         name={input.name} 
                         className="input"
                         type={input.type}
                         {...register(input.label)}
                         />
+                    </div>    
                     </p>
                   </div>
                 );
             })}
-            <button className="btn" type="submit">
-                submit
-            </button>           
+            <button className="btn" type="SUBMIT">
+                SUBMIT
+            </button>     
+        </div>      
         </form>
+
+        <div className="SignupImage">
+        <img src={img4}/>
+        </div>
+
         </div>
     );
 };
