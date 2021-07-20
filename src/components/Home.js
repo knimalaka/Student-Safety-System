f3a781a3ac952e88a330f2f8bd8743be919bc0cf
knimalaka <kavindanimalaka97@gmail.com>
2021-07-20 10:35:20 +0530

    Adding styles to QRcode generator

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,11 +5,13 @@ import {Container, Card, CardContent, makeStyles, Grid, TextField, Button} from 
 import QRCode from 'qrcode';
 
 function Home (){
+  const [text, setText] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
 
   const generateQrCode = async () =>{
     try{
-      const response = await QRCode.toDataURL('Test QR');
-      console.log(response); 
+      const response = await QRCode.toDataURL(text);
+      setImageUrl(response); 
     }catch(error){
       console.log(error);
     }
@@ -23,10 +25,21 @@ function Home (){
       <h2>Home</h2>
         <CardContent>
           <Grid container spacing={2}>
-            <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
-              <TextField label="Enter Text Here"/>
+            <Grid className="QRcode" item xl={4} lg={4} md={6} sm={12} xs={12}>
+              <TextField label="Enter Text Here" onChange={(e) => setText(e.target.value)}/>
               <Button className="qrButton" variant="contained" 
               color="primary" onClick={() => generateQrCode()}>Generate</Button>
+              <br/>
+              <br/>
+              <br/>
+              {imageUrl ? (
+                <a href={imageUrl} download>
+                <img src={imageUrl} alt="img"/>
+                </a>) : null}
+              <br/>
+              <br/>
+              <Button className="QRdownload" variant="contained" 
+              color="primary" href={imageUrl} download>Download</Button>
             </Grid>
             <Grid item xl={4} lg={4} md={6} sm={12} xs={12}></Grid>
             <Grid item xl={4} lg={4} md={6} sm={12} xs={12}></Grid>
