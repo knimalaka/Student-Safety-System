aa8027848f316912c1be9289a0849bfd44d0bdc4
knimalaka <kavindanimalaka97@gmail.com>
2021-07-21 12:28:38 +0530

    path Error solved

--- src/components/Frontpage.js
+++ src/components/Frontpage.js
@@ -33,7 +33,7 @@ let history = useHistory();
           </div>
   
           <div className="Parent">
-          <button onClick= {() => {history.push("./components/User/UserFrontPage"); }}>
+          <button onClick= {() => {history.push("./components/UserFrontPage"); }}>
           <div className="imageParent"><img src={img2}/><p>Parent</p></div></button>
           </div>
   
