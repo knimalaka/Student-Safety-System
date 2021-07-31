a72772bab98122d2dfe63e18322cb4de72986911
knimalaka <kavindanimalaka97@gmail.com>
2021-07-31 21:42:07 +0530

    Database connected and testind with postman

--- server/app.js
+++ server/app.js
@@ -1,20 +1,41 @@
 const express = require("express");
 const app = express();
 const student =  require("./routes/student");
-
 const mysql = require("mysql");
+const bodyparser = require("body-parser");
 
-
+var mysqlConnection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'studentsafetydb'
+});
 
 app.use(express.json());
 
 app.use("/student", student);
 
 app.get("/", (req, res)=>{
-    console.log(req.url);
-    res.send("<h1>Hello</h1>");
+  console.log(req.url);
+  res.send("<h1>Hello</h1>");
 });
 
+mysqlConnection.connect((err) => {
+  if(!err)
+    console.log('DB connected successfully !');
+  else
+    console.log('DB connection failed \n Error :'+ JSON.stringify(err, undefined, 2));
+});
+
+app
+  .get('/students', (req, res) =>{
+    mysqlConnection.query('SELECT * FROM students', (err, rows, fields) =>{
+      if(!err)
+      res.send(rows);
+      else
+      console.log(err);
+    })
+  });
 
 app.listen(4000, (err)=>{
   if (err) {
--- server/routes/student.js
+++ server/routes/student.js
@@ -1,5 +1,15 @@
 "use strict";
 const express = require("express");
+const mysql = require("mysql");
+
+
+var mysqlConnection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'studentsafetydb'
+  });
+
 let router = express.Router();
 
 router.use(function(req, res, next){
@@ -8,12 +18,28 @@ router.use(function(req, res, next){
 });
 
 router
+  .get('/students', (req, res) =>{
+    mysqlConnection.query('SELECT * FROM students', (err, rows, fields) =>{
+      if(!err)
+      res.send(rows);
+      else
+      console.log(err);
+    })
+  });
+
+router
   .route("/register")
   .get((req, res)=>{
       res.send("hi get register here");
   })
   .post((req,res)=>{
-      res.send("hi post register here");
-  });
+    mysqlConnection.query('INSERT INTO students (Class, Email, First_Name, Parents_Name, Parents_Phone_Number, Password, Second_Name, Student_ID) VALUES (?,?,?,?,?,?,?,?)', (err, rows, fields) =>{
+        if(!err)
+        res.send(rows);
+        else
+        console.log(err);
+      })  
+});
+
 
 module.exports = router; 
